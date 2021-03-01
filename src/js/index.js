d054ec66262d39e41071db9d51871af3081c780c
Milan Sacher <milan.lanlada@gmail.com>
2021-03-01 00:34:25 -0500

    style lp

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
-const navBtn = document.querySelector(".js-nav-btn");
-const menu = document.querySelector(".menu__list");
+const navBtn = document.querySelector(".js-nav-btn"),
+  menu = document.querySelector(".menu__list"),
+  body = document.querySelector("body");
 
 function toggleMenu() {
   if (window.innerWidth < 768) {
     navBtn.classList.toggle("menu__btn_close");
     menu.classList.toggle("menu__list-active");
+    body.classList.toggle("has-modal-open");
   }
 }
 
